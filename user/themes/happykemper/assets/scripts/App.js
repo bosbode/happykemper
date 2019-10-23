bee2760303b5dc76ce4ef09632a4b53f194dac30
Bosse Kemper <bosbode@gmail.com>
2019-10-23 15:57:23 +0200

    Update dependencies

--- user/themes/happykemper/assets/scripts/App.js
+++ user/themes/happykemper/assets/scripts/App.js
@@ -1,10 +1,10 @@
 import OutdatedBrowser from "./modules/OutdatedBrowser";
 import Glide from "./modules/Glide";
 import Animations from "./modules/Animation";
-// import Video from "./modules/Video";
+import Video from "./modules/Video";
 
 
 let outdatedBrowser = new OutdatedBrowser;
 let glide = new Glide;
 let animations = new Animations();
-// let video = new Video();
+let video = new Video();
--- user/themes/happykemper/assets/scripts/Vendor.js
+++ user/themes/happykemper/assets/scripts/Vendor.js
@@ -1,3 +1,4 @@
 // import "../compiled/scripts/modernizr";
 import "lazysizes";
+import 'lazysizes/plugins/unveilhooks/ls.unveilhooks';
 // import "picturefill";
